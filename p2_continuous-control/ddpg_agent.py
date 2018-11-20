82c8a8069a72d21b06a6d61531fa3b5d4939e7d3
Marifel Barbasa <mrbarbasa@gmail.com>
2018-11-20 13:52:07 -1000

    Set default parameters and adjust the print format

--- p2_continuous-control/ddpg_agent.py
+++ p2_continuous-control/ddpg_agent.py
@@ -13,13 +13,13 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
-BUFFER_SIZE = int(1e6)  # replay buffer size # TODO 1e5
+BUFFER_SIZE = int(1e5)  # replay buffer size # TODO 1e6
 BATCH_SIZE = 128        # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
 LR_ACTOR = 1e-4         # learning rate of the actor 
-LR_CRITIC = 3e-4        # learning rate of the critic # TODO 1e-3
-WEIGHT_DECAY = 0.0001   # L2 weight decay # TODO 0
+LR_CRITIC = 1e-3        # learning rate of the critic # TODO 3e-4
+WEIGHT_DECAY = 0        # L2 weight decay # TODO 0.0001
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
