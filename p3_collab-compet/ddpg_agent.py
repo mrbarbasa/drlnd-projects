ad19117664bd5ba68aa2f5b84a798e25643fdbd6
Marifel Barbasa <mrbarbasa@gmail.com>
2018-11-24 17:09:05 -1000

    Trial 11: Try with actor learning rate of 1e-3

--- p3_collab-compet/ddpg_agent.py
+++ p3_collab-compet/ddpg_agent.py
@@ -14,10 +14,10 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 BUFFER_SIZE = int(1e5)  # replay buffer size
-BATCH_SIZE = 64         # minibatch size
+BATCH_SIZE = 128        # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 3e-1              # for soft update of target parameters
-LR_ACTOR = 1e-4         # learning rate of the actor 
+LR_ACTOR = 1e-3         # learning rate of the actor 
 LR_CRITIC = 1e-3        # learning rate of the critic
 WEIGHT_DECAY = 0        # L2 weight decay
 
